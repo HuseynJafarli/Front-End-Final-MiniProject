895d5803fb1e9af858edf92fddc4d1c4cab4130b
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2024-08-06 16:08:55 +0400

    Main section done. Footer banner in progress.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,54 @@
 (function () {
-    const second = 1000,
-          minute = second * 60,
-          hour = minute * 60,
-          day = hour * 24;
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "08/30/",
-        birthday = dayMonth + yyyy;
-    
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-      birthday = dayMonth + nextYear;
-    }
-    //end
-    
-    const countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {    
-  
-          const now = new Date().getTime(),
-                distance = countDown - now;
-  
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-          if (distance < 0) {
-            clearInterval(x);
+  const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      dayMonth = "12/30/",
+      countDownDate = new Date(dayMonth + yyyy).getTime();
+
+  today = mm + "/" + dd + "/" + yyyy;
+
+  let x = setInterval(function () {
+      const now = new Date().getTime(),
+          distance = countDownDate - now;
+
+      let daysElements = document.getElementsByClassName("days");
+      let hoursElements = document.getElementsByClassName("hours");
+      let minutesElements = document.getElementsByClassName("minutes");
+      let secondsElements = document.getElementsByClassName("seconds");
+
+      for (let i = 0; i < daysElements.length; i++) {
+          daysElements[i].innerText = Math.floor(distance / day);
+      }
+      for (let i = 0; i < hoursElements.length; i++) {
+          hoursElements[i].innerText = Math.floor((distance % day) / hour);
+      }
+      for (let i = 0; i < minutesElements.length; i++) {
+          minutesElements[i].innerText = Math.floor((distance % hour) / minute);
+      }
+      for (let i = 0; i < secondsElements.length; i++) {
+          secondsElements[i].innerText = Math.floor((distance % minute) / second);
+      }
+
+      if (distance < 0) {
+          clearInterval(x);
+          for (let i = 0; i < daysElements.length; i++) {
+              daysElements[i].innerText = "0";
           }
-        }, 0)
-    }());
+          for (let i = 0; i < hoursElements.length; i++) {
+              hoursElements[i].innerText = "0";
+          }
+          for (let i = 0; i < minutesElements.length; i++) {
+              minutesElements[i].innerText = "0";
+          }
+          for (let i = 0; i < secondsElements.length; i++) {
+              secondsElements[i].innerText = "0";
+          }
+      }
+  }, 1000);
+})();
